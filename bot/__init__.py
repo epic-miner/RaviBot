976067d9a0ad6f950920c55a415d2da3eeaeec98
Ravi Gurjar <99577613+RaviHackjr@users.noreply.github.com>
2025-02-12 19:39:44 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -61,7 +61,7 @@ class Var:
     DEL_TIMER = int(getenv("DEL_TIMER", "600"))
     START_PHOTO = getenv("START_PHOTO", "https://te.legra.ph/file/120de4dbad87fb20ab862.jpg")
     START_MSG = getenv("START_MSG", "<b>Hey {first_name}</b>,\n\n    <i>I am Auto Animes Store & Automater Encoder Build with ❤️ !!</i>")
-    START_BUTTONS = getenv("START_BUTTONS", "UPDATES|https://telegram.me/NineAnimeOfficial SUPPORT|https://t.me/BlakiteFF")
+    START_BUTTONS = getenv("START_BUTTONS", "UPDATES|https://telegram.me/Matiz_Tech SUPPORT|https://t.me/+p78fp4UzfNwzYzQ5")
 
 if Var.THUMB and not ospath.exists("thumb.jpg"):
     system(f"wget -q {Var.THUMB} -O thumb.jpg")
